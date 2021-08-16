fd0f78897aa7f557ad88b31e7d27cc4aa112c1a4
omerbd21 <68389553+omerbd21@users.noreply.github.com>
2021-08-16 12:08:43 +0300

    Update app.py

--- python/app.py
+++ python/app.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, Response
+from routes import index, document
+from elasticsearch import Elasticsearch
+
 
 app = Flask(__name__)
+
+with app.app_context():
+    es = Elasticsearch()
+    
+    
+app.register_blueprint(index)
+app.register_blueprint(document)
 
 @app.route("/")
 def index():
     return "Hello to my Elastic app!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
