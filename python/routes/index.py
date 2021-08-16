2433676c32b5b021fa16b624282d6910a4db524c
omerbd21 <68389553+omerbd21@users.noreply.github.com>
2021-08-16 12:42:20 +0300

    Update index.py

--- python/routes/index.py
+++ python/routes/index.py
@@ -1,25 +1,30 @@
 from flask import Blueprint, Response, json, request
 
-from crud import get_indices, get_index, post_index, delete_index
+from crud import get_indices, get_index, put_index, post_index, delete_index
 
 index = Blueprint('index', __name__)
 
-@website.route('/index')
+@index.route('/index')
 def fetch_indices():
     return Response(json.dumps(get_indices()), 200)
   
   
-@website.route('/index/<index_name>')
+@index.route('/index/<index_name>')
 def fetch_index(index_name):
     return Response(json.dumps(get_index(index_name)), 200)
+
+@index.route('/index')
+def update_index(index_name):
+    response = put_index(request.get_json())
+    return Response(json.dumps(put_index(reponse["name"], response["scheme"])), 200)
   
   
-@website.route('/index')
+@index.route('/index')
 def create_index():
     response = post_index(request.get_json())
     return Response(json.dumps(response), 200)
   
   
-@website.route('/names/<index_name>')
+@index.route('/names/<index_name>')
 def remove_index(index_name):
     return Response(json.dumps(delete_index(index_name)), 200)
