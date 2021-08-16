52efb58b12610679da16087ac06b19eb15988de5
omerbd21 <68389553+omerbd21@users.noreply.github.com>
2021-08-16 12:48:55 +0300

    Update document.py

--- python/routes/document.py
+++ python/routes/document.py
@@ -1,1 +1,25 @@
+from flask import Blueprint, Response, json, request
 
+from crud import get_documents, get_document, post_document, delete_document
+
+document_actions = Blueprint('document_actions', __name__)
+
+@document.route('/document')
+def fetch_documents():
+    return Response(json.dumps(get_documents()), 200)
+  
+  
+@document.route('/document/<document_name>')
+def fetch_document(document_name):
+    return Response(json.dumps(get_document(document_name)), 200)
+  
+  
+@document.route('/document',methods = ['POST'])
+def create_document():
+    response = post_document(request.get_json())
+    return Response(json.dumps(response), 200)
+  
+  
+@document.route('/document/<document_name>',methods = ['DELETE'])
+def remove_document(document_name):
+    return Response(json.dumps(delete_document(document_name)), 200)
