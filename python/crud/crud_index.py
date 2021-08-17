c3a2b9ba8f74a316fa8d8463db2e72271a6e802e
omerbd21 <68389553+omerbd21@users.noreply.github.com>
2021-08-17 15:15:47 +0300

    Update crud_index.py

--- python/crud/crud_index.py
+++ python/crud/crud_index.py
@@ -1,1 +1,16 @@
+from elasticsearch import Elasticsearch
 
+
+def get_indices():
+  return current_app.es.indices.get_alias("*")
+
+
+def get_index(index_name):
+  index = current_app.es.search(index=index_name, body={"query": {"match_all": {}}})
+  return index
+
+def post_index(index_name, index_schema):
+  return current_app.es.indices.create(index = index_name, body = index_schema)
+
+def delete_index(index_name):
+  return current_app.es.indices.delete(index=index_name, ignore=[400, 404])
