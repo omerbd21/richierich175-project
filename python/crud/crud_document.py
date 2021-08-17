1af40a43f70b909a7306a3c7f42ed87279779c96
omerbd21 <68389553+omerbd21@users.noreply.github.com>
2021-08-17 15:27:25 +0300

    Update crud_document.py

--- python/crud/crud_document.py
+++ python/crud/crud_document.py
@@ -1,1 +1,18 @@
+from elasticsearch import Elasticsearch
 
+
+def get_documents(index_name):
+  res = current_app.es.search(index=index_name, body={"query": {"match_all": {}}})
+  return [doc["_source"] for doc in res["hits"]["hits"]]
+
+
+def get_document(index_name, doc_id):
+  res = current_app.es.get(index=index_name, id=doc_id)
+  return res["_source"]
+
+def post_document(index_name, doc_id, doc_body):
+  res = current_app.es.index(index=index_name, id=doc_id, body=doc_body)
+  return res['result']
+
+def delete_document(index_name, doc_id):
+  return current_app.es.delete(index=index_name, id=doc_id)
