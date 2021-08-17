08853145e0fff93d3cdf07a5785bfff2d6ee60d6
omerbd21 <68389553+omerbd21@users.noreply.github.com>
2021-08-17 15:27:18 +0300

    Update document.py

--- python/routes/document.py
+++ python/routes/document.py
@@ -9,17 +9,19 @@ def fetch_documents(index_name):
     return Response(json.dumps(get_documents(index_name)), 200)
   
   
-@document.route('/document/<document_id>')
-def fetch_document(document_id):
-    return Response(json.dumps(get_document(document_id)), 200)
+@document.route('/document/<index_name>/<document_id>')
+def fetch_document(index_name, document_id):
+    return Response(json.dumps(get_document(index_name, document_id)), 200)
   
   
 @document.route('/document',methods = ['POST'])
 def create_document():
-    response = post_document(request.get_json())
-    return Response(json.dumps(response), 200)
+    index_name = request.get_json()["index"]
+    doc_id = request.get_json()["id"]
+    doc_body = request.get_json()["body"]
+    return Response(json.dumps(post_document(index_name, doc_id, doc_body)), 200)
   
   
-@document.route('/document/<document_name>',methods = ['DELETE'])
-def remove_document(document_name):
-    return Response(json.dumps(delete_document(document_name)), 200)
+@document.route('/document/<index_name>/<doc_id>',methods = ['DELETE'])
+def remove_document(index_name, doc_id):
+    return Response(json.dumps(delete_document(index_name, doc_id)), 200)
