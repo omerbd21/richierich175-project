468244612988d08f5b5c6f9d4a4be167ef8dcc00
omerbd21 <68389553+omerbd21@users.noreply.github.com>
2021-08-18 11:54:08 +0300

    Update app.py

--- python/app.py
+++ python/app.py
@@ -2,19 +2,23 @@ from flask import Flask, Response
 from routes import index_actions, document_actions
 from elasticsearch import Elasticsearch
 
-
+# Creates a flask instance
 app = Flask(__name__)
 
+
+# uses app context to run elasticsearch api in python. this is usable all through api with current_app.es
 with app.app_context():
     es = Elasticsearch()
     
-    
+# Registring blueprints, which contains all the api routes for each object
 app.register_blueprint(index_actions)
 app.register_blueprint(document_actions)
 
+# Basic route for index
 @app.route("/")
 def index():
     return "Welcome to my Elastic app!"
 
+# run the flask app on localhost in 8080
 def run():
     app.run(host='0.0.0.0', port=8080)
